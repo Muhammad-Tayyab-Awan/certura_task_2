944a0fb18bd9649c2c736a945be89f7d982ec11e
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-05-06 16:14:28 +0500

    added static files plus not-found template file

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const http = require("http");
+const path = require("path");
 const socketIO = require("socket.io");
 
 const app = express();
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 const io = socketIO(server);
+
+app.use(express.static("public"));
+
+app.all(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "views/not-found.html"));
+});
 
 server.listen(port, () => {
   console.clear();
